6fdf5f70bee3295b67503e8002805f4c5a9a579a
ShitpostBot <brett.orange.memes@gmail.com>
2015-11-06 01:23:35 +1100

    removed padding
    this is good enough for now, me or someone else can change it later

--- spb/header.php
+++ spb/header.php
@@ -10,7 +10,7 @@
 	<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
 </head>
 <body>
-<div class="container-fluid padded orange">
+<div class="container-fluid orange">
 	<nav class="navbar navbar-orange">
 	<div class="container-fluid">
 		<div class="navbar-header">
