1e27a70cb8228a1ad118d0ff924018b534f08425
ShitpostBot <brett.orange.memes@gmail.com>
2015-11-08 22:36:18 +1100

    moved database class to it's own file

--- spb/dataaccess/loader.php
+++ spb/dataaccess/loader.php
@@ -31,25 +31,4 @@ function addCategoriesComboBox($id, $class = 'categories'){
     }
     echo '</select>';
 }
-
-class Database {
-        public function __construct($name) {
-                $this->d = new SQLite3($name);
-        }
-        public function query($query) {
-                return $this->d->query($query);
-        }
-        public function queryOne($query) {
-                $a = $this->query($query);
-                return $a->fetchArray(SQLITE3_NUM);
-        }
-        public function queryFirstEach($query) {
-                $a = $this->query($query);
-                $b = array();
-                while ($c = $a->fetchArray(SQLITE3_NUM)) {
-                        $b[] = $c[0];
-                }
-                return $b;
-        }
-}
-
+?>
