f0f94fdcf59de77ede90b31113b6a3458dac708d
ShitpostBot <brett.orange.memes@gmail.com>
2015-12-11 03:06:03 +1100

    removed pointless testing code

--- spb/sqltest.php
+++ spb/sqltest.php
@@ -1,7 +1,5 @@
 <?php
 include('php/autoload.php');
-$db = new Database();
-$loginmsg = $db->login('zeroth', 'memes');
 
-echo $twig->render('sqltest.html', array('loginmsg' => $loginmsg));
+echo $twig->render('sqltest.html', array());
 ?>
