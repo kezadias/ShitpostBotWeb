2eaa87dbca24e4dc60179280d84af166cb3f382a
ShitpostBot <brett.orange.memes@gmail.com>
2015-12-16 13:01:49 +1100

    fixed stupid mistake
    im gonna take a break from this project for a little bit, i'm making so
    many silly mistakes

--- spb/do-review.php
+++ spb/do-review.php
@@ -10,16 +10,22 @@ $t = $_GET['t']; //type
 
 if($s === 'a' && $t === 't'){
 	echo $db->acceptTemplate($id);
+	
 } elseif($s === 'd' && $t === 't'){
 	echo $db->denyTemplate($id);
+	
 } elseif($s === 'a' && $t === 's'){
 	echo $db->acceptSourceImage($id);
+	
 } elseif($s === 'd' && $t === 's'){
 	echo $db->denySourceImage($id);
+	
 } elseif($s === 'p' && $t === 't'){
 	echo $db->makeTemplatePending($id);
+	
 } elseif($s === 'p' && $t === 's'){
 	echo $db->makeSourceImagePending($id);
+	
 } else{
 	echo ';failed';
 }
