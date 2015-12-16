0290ce8ada4893e31a11b3b20d6043571df159a1
ShitpostBot <brett.orange.memes@gmail.com>
2015-12-16 12:45:28 +1100

    added buttons on view page to deny/make pending

--- spb/js/review.js
+++ spb/js/review.js
@@ -1,11 +1,17 @@
 $(document).ready(function(){
 	$('.review').click(function(e){
+		var btn = $(this);
 		var id = $(this).attr('code');
 		var type = $(this).attr('type');
 		var state = $(this).attr('state');
 		$.get('do-review.php?id='+id+'&t='+type+'&s='+state, function(data){
 			if(data == ';success'){
-				location.reload();
+				var ret = btn.attr('return');
+				if(typeof ret !== typeof undefined && ret !== false){
+					window.location.href = ret;
+				} else{
+					location.reload();
+				}
 			}else{
 				alert(translate(data));
 			}
