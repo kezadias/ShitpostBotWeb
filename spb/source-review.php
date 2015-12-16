4dc8d38fe6f46f0314000062723db107435baeb4
ShitpostBot <brett.orange.memes@gmail.com>
2015-12-16 18:12:21 +1100

    added timestamp to content review

--- spb/source-review.php
+++ spb/source-review.php
@@ -1,12 +1,10 @@
 <?php
 include('php/autoload.php');
-$row = $db->query("SELECT u.userId as submitterId, u.username as submitterName, s.sourceId, s.sourceId || '.' || s.filetype as file
+$row = $db->query("SELECT u.userId as submitterId, s.timeAdded as timeAdded, u.username as submitterName, s.sourceId, s.sourceId || '.' || s.filetype as file
 				   FROM SourceImages as s, Users as u
 				   WHERE u.userId = s.userId AND s.reviewState = 'p'
 				   ORDER BY random()
-				   LIMIT 1",
-				   array(),
-				   array())->fetchArray();
+				   LIMIT 1")->fetchArray();
 
 $count = $db->scalar("SELECT count(*) FROM SourceImages WHERE reviewState = 'p'", array(), array());
 echo $twig->render('source-review.html', $row === false ? array('msg' => 'No templates found.') : array_merge($row, array('count' => $count)));
--- spb/template-review.php
+++ spb/template-review.php
@@ -1,12 +1,10 @@
 <?php
 include('php/autoload.php');
-$row = $db->query("SELECT u.userId as submitterId, u.username as submitterName, t.templateId, t.templateId || '.' || t.filetype as file, CASE WHEN t.overlayFiletype = null THEN null ELSE t.templateId || '.' || t.overlayFiletype END as overlayFile
+$row = $db->query("SELECT u.userId as submitterId, u.username as submitterName, t.templateId, t.timeAdded as timeAdded, t.templateId || '.' || t.filetype as file, CASE WHEN t.overlayFiletype = null THEN null ELSE t.templateId || '.' || t.overlayFiletype END as overlayFile
 				   FROM Templates as t, Users as u
 				   WHERE u.userId = t.userId AND t.reviewState = 'p'
 				   ORDER BY random()
-				   LIMIT 1",
-				   array(),
-				   array())->fetchArray();
+				   LIMIT 1")->fetchArray();
 
 $count = $db->scalar("SELECT count(*) FROM Templates WHERE reviewState = 'p'", array(), array());
 echo $twig->render('template-review.html', $row === false ? array('msg' => 'No templates found.') : array_merge($row, array('count' => $count)));
